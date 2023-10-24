b86e6fc0c9634d9718f8d0bfd12a33e6888652ee
Andrew Healey <doolie.healey@gmail.com>
2023-10-24 16:59:44 +0100

    Update testcase

--- rotation.py
+++ rotation.py
@@ -4,10 +4,12 @@ from math import pi
 
 v = np.array([ 6,4,2 ])
 
-r = Quaternion.from_axis_angle(np.array([0,1,0]),0)#pi/2)
+r = Quaternion.from_axis_angle(np.array([0,1,0]),pi/2)
 print(r)
 translation = np.array([50,40,30])
 
 q = DualQuaternion.from_rot_trans(r,translation)
 
-print(q.transform_vector(v)) # [50,40,1]
+q = DualQuaternion.from_rot_trans(r,translation)
+
+print(q.transform_vector(v)) # [48,44,36]
